0825f55ecdda47faddc81c92a7278c84eaf99c0f
quiz-app <mgovind6058@gmail.com>
2026-01-17 02:08:56 +0530

    updated error

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ import profileRoutes from "./routes/profile.js";
 import projectRoutes from "./routes/projects.js";
 
 dotenv.config();
-
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
