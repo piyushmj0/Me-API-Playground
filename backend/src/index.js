9a9d295121178a0052424deb983b3534d8f6eac8
quiz-app <mgovind6058@gmail.com>
2026-01-17 02:20:21 +0530

    backend url issue solved

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,8 +7,9 @@ import profileRoutes from "./routes/profile.js";
 import projectRoutes from "./routes/projects.js";
 
 dotenv.config();
-const PORT = process.env.PORT || 3000;
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -16,6 +17,8 @@ app.use("/health", healthRoutes);
 app.use("/profile", profileRoutes);
 app.use("/projects", projectRoutes);
 
-app.listen(process.env.PORT, () =>
-  console.log("Server running")
-);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
